74a95148cd4d8b998e7e4fddae56d619ba3dd94a
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2023-10-26 14:55:28 +0100

    added string worm boss

--- Toy Game/Assets/Scripts/Map/BossNode.cs
+++ Toy Game/Assets/Scripts/Map/BossNode.cs
@@ -9,13 +9,14 @@ namespace Map {
     {
         public Creature boss;
         public List<Creature> minionPool;
+        public int startingMinions;
         public int minionsPerLoop;
 
         public override List<Creature> GetEnemies() {
             List<Creature> list = new List<Creature>();
 
             list.Add(boss);
-            int minions = minionsPerLoop*(Manager.instance.bossesKilled+1);
+            int minions = startingMinions + minionsPerLoop*Manager.instance.bossesKilled;
 
             for (int i = 0; i < minions; i++) {
                 list.Add(minionPool[Random.Range(0, minionPool.Count)]);
