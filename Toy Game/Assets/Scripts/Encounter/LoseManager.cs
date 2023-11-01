85e1956ef19f702b193e74b8434b64ab9a855a07
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2023-11-01 12:19:05 +0000

    added death music

--- Toy Game/Assets/Scripts/Encounter/LoseManager.cs
+++ Toy Game/Assets/Scripts/Encounter/LoseManager.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 namespace Encounter {
     public class LoseManager : MonoBehaviour
     {
+        [SerializeField] private AudioClip deathMusic;
+
+        private void Start() {
+            Game.instance.Play(deathMusic);
+        }
+
         public void Quit() {
             Game.instance.Quit();
         }
