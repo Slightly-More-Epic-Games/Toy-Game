426e429fc7873a01129620c1c5fe4d699da60b81
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2023-11-01 11:41:23 +0000

    quit button no longer shows up in webgl

--- Toy Game/Assets/Scripts/MainMenu.cs
+++ Toy Game/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject standaloneButtons;
+    [SerializeField] private GameObject webglButtons;
+
+    private void Awake() {
+        #if UNITY_WEBGL 
+        webglButtons.SetActive(true);
+        standaloneButtons.SetActive(false);
+        #else
+        webglButtons.SetActive(false);
+        standaloneButtons.SetActive(true);
+        #endif
+    }
+
     public void LoadGame() {
         SceneManager.LoadScene("Game");
     }
